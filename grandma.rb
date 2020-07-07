2c782fbb097a823aa6e79f7dd7225d131a4177d0
Carter Parrett <cparrett94@hotmail.com>
2020-07-07 07:15:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
  def speak_to_grandma(phrase)
    if phrase.upcase == phrase
      return "NO, NOT SINCE 1938!"
+   elsif "I LOVE YOU GRANDMA!"
    else 
      return "HUH?! SPEAK UP, SONNY!"
    end
